a89b470bbe1df76d7744a210d71d4c5bbb18d507
Jake Collins <jakcllns@gmail.com>
2021-01-02 20:22:41 -0500

    Created NavBar component for desktop Next: * Create sign up page * Add in routing with react router * Create mobile NavBar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import Button from '@material-ui/core/Button';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import NavBar from './components/NavBar/navBar';
+import { useState } from 'react';
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        light: '#ffffff',
+        main: '#ffffff',
+        dark: '#cccccc',
+        contrastText: '#000000'
+      },
+      secondary: {
+        light: '#438141',
+        main: '#115417',
+        dark: '#002a00',
+        contrastText: '#ffffff'
+      },
+      test: {
+        light: '#6a96ff',
+        main: '#2569d6',
+        dark: '#003fa4',
+        contrastText: '#ffffff'
+      }
+    },
+  });
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  
+  const handleSignOut = event => {
+    //Add clean up code for once routing is added
+    setIsAuth(false);
+  }
+  
+  const handleLogin = event => {
+    //Authentication code once we start adding the backend in
+    setIsAuth(true);
+  }
+
   return (
-    <div className="App">
-      <Button variant="contained" color="primary">I'm a button</Button>
-      <h1>Start Project</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <header>
+        <NavBar 
+          isAuthenticated={isAuth}
+          onSignOut={handleSignOut}
+          onLogin={handleLogin}
+        />
+      </header>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/NavBar/navBar.js
@@ -0,0 +1,114 @@
+import { 
+    AppBar, 
+    Toolbar, 
+    Typography, 
+    IconButton ,
+    Menu,
+    MenuItem,
+    Link,
+    makeStyles,
+    Button,
+} from "@material-ui/core";
+
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import { useState } from 'react';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        margin: theme.spacing(0, 2, 0, 2 )
+    },
+    link: {
+        margin: theme.spacing(1, 1.5),
+    },
+    toolbar: {
+        flexWrap: 'wrap',
+    },
+    toolbarTitle: {
+        flexGrow: 1,
+    },
+}));
+
+const NavBar = props => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const auth = props.isAuthenticated;
+    const open = Boolean(anchorEl);
+    const classes = useStyles();
+
+    const handleClose = event => {
+        setAnchorEl(null);    
+    }
+
+    const handleMenu =  event => {
+        setAnchorEl(event.currentTarget);
+    }
+
+    return (
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography 
+                    variant="h6" 
+                    noWrap 
+                    color="inherit" 
+                    className={classes.toolbarTitle}
+                >
+                    My Website
+                </Typography>
+                <nav>
+                    <Link variant="button" color="inherit" href="/" className={classes.link}>Home</Link>
+                    <Link variant="button" color="inherit" href="/about" className={classes.link}>About Us</Link>
+                    <Button 
+                        variant="contained"
+                        href="/signup"
+                        color="secondary"
+                        
+                    >
+                        Sign Up
+                    </Button>
+                    <IconButton
+                        aria-label="current user account"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleMenu}
+                        color="secondary"
+                    >
+                        <AccountCircle />
+                    </IconButton>
+                    
+                        {auth && (
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal:'right',
+                                }}
+                                open={open}
+                                onClose={handleClose}
+                            >
+                                <MenuItem>Profile</MenuItem>
+                                <MenuItem>My Posts</MenuItem>
+                                <MenuItem component="button" onClick={props.onSignOut} >Sign Out</MenuItem>
+                            </Menu>
+                        )}
+                        {!auth && (
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal:'right',
+                                }}
+                                open={open}
+                                onClose={handleClose}
+                            >
+                                <MenuItem component="button" onClick={props.onLogin}>Login</MenuItem>
+                            </Menu>
+                        )}
+                </nav>
+                
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default NavBar;
